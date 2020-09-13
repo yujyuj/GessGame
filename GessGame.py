e778c84ca2ca424186c0dae0fccc4c8561d08915
yujyuj <movaxbxjmp@gmail.com>
2020-09-13 14:07:16 -0400

    updates

--- GessGame.py
+++ GessGame.py
@@ -457,3 +457,7 @@ class GessGame:
             
         print('-' * 81)
         print('  a    b   c   d   e   f   g   h   i   j   k   l   m   n   o   p   q   r   s   t')
+
+
+# game = GessGame()
+# game.print_board()
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # GessGame
+python
